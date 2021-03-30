a2871f9ff4d5a16992cf00da4ed887270597e80d
takahashi riki <r.takahashi.sys20@morijyobi.ac.jp>
2021-03-30 12:09:02 +0900

    円周の計算を追加しました！
    enn.html及びkekka.htmlを追加しました
    頑張りました

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def top():
     return render_template("index.html")
+
+@app.route("/enn")
+def enn():
+    return render_template("enn.html")
+
+@app.route("/kekka")
+def kekka():
+    e = int(request.args.get("en"))
+    r = (e+e)*3.14
+    m = e*e*3.14
+    return render_template("kekka.html",r=r,m=m)
 
 if __name__ == "__main__":
     app.run(debug=True)
